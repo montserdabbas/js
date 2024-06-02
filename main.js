6c631bc98a5d54ac0db90c18f89ed3148a722c5a
montserdabbas <montaser2002dabbas@gmail.com>
2024-06-02 21:43:58 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+//document.querySelector ("h2");
+document.querySelector ("h2").textContent = " hello montaser";
+//document.querySelector (".userInfo p");
+
+document.querySelector (".userInfo p").classList.add ("dark");
